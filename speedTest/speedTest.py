0f753cf0d9506cf9f879330c0afadfaf95abb270
Luciano Junior <108093135+LuciannoJr@users.noreply.github.com>
2025-04-16 20:06:23 -0300

    Add files via upload

---
+++ speedTest/speedTest.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import speedtest
+
+def main():
+    st.header("SpeedTest", divider=True)
+    st.write("Clique no botão abaixo para iniciar o teste.")
+
+    if st.button("Iniciar"):
+        with st.spinner("Testeando a velocidade da sua internet..."):
+            s = speedtest.Speedtest()
+            s.get_best_server()
+            download_speed = s.download() / 1_000_000
+            upload_speed = s.upload() / 1_000_000
+            results = s.results.dict()
+
+            max_speed = 150
+            st.write(f"Velocidade de Download: {download_speed:.2f} Mbps")
+            st.progress(min(download_speed / max_speed, 1.0))
+
+            st.write(f"Ping: {results['ping']} ms")
+
+main()
